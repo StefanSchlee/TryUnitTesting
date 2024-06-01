9c0c8ecdeb23a98f79f7c00abd074dbe4e782a59
StefanS <75912519+StefanSchlee@users.noreply.github.com>
2024-06-01 20:16:55 +0200

    tox running

--- tests/MyModule_test.py
+++ tests/MyModule_test.py
@@ -1,6 +1,15 @@
 from MyPackage.MyModule import SquareFcn
 import pytest
 
-def test_Square():
-    assert(SquareFcn(3) == 9)
-    print("Test succeded!")
+@pytest.mark.parametrize("Input", [int(-5), -3.4, 0, 0.2, 3, 4.4])
+def test_Square_positives(Input):
+    assert SquareFcn(Input) == Input*Input
+    assert SquareFcn(Input) == Input**2
+
+@pytest.mark.skip(reason="Input checking not yet implementet!")
+@pytest.mark.parametrize("Input", [True, "hello", [3,2]])
+def test_Square_negatives(Input):
+    with pytest.raises(ValueError):
+        SquareFcn(Input)
+
+# def test_Square_OnArray
